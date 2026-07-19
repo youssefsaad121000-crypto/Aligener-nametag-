811f54bf6a44a906e3f589755abf0f859097267b
youssefsaad121000-crypto <youssefsaad121000@gmail.com>
2026-07-19 12:26:14 +0300

    Update EntityRendererMixin.java

--- src/main/java/com/textaligner/mixin/EntityRendererMixin.java
+++ src/main/java/com/textaligner/mixin/EntityRendererMixin.java
@@ -1,1 +1,44 @@
+package com.textaligner.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin<T extends Entity> {
+
+    @Inject(method = "renderLabelIfPresent", at = @At("HEAD"))
+    private void textAligner$beforeLabel(
+            T entity,
+            Text text,
+            MatrixStack matrices,
+            VertexConsumerProvider vertexConsumers,
+            int light,
+            CallbackInfo ci
+    ) {
+        RenderSystem.enableDepthTest();
+        RenderSystem.depthMask(true);
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+    }
+
+    @Inject(method = "renderLabelIfPresent", at = @At("RETURN"))
+    private void textAligner$afterLabel(
+            T entity,
+            Text text,
+            MatrixStack matrices,
+            VertexConsumerProvider vertexConsumers,
+            int light,
+            CallbackInfo ci
+    ) {
+        RenderSystem.enableDepthTest();
+        RenderSystem.depthMask(true);
+    }
+}
